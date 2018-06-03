07f5d7e1da311d936c75faa8559a78108b194327
Zoltan Csenyi <csoltan422@gmail.com>
2018-06-03 13:26:05 +0200

    Added h2 db to classpath, mongodb and jasypt configured

--- src/main/java/org/zenbot/timetableupdater/TimetableUpdaterApplication.java
+++ src/main/java/org/zenbot/timetableupdater/TimetableUpdaterApplication.java
@@ -1,9 +1,12 @@
 package org.zenbot.timetableupdater;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class TimetableUpdaterApplication {
 
 	public static void main(String[] args) {
