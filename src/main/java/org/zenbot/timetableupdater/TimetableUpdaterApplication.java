4ba64a6590951d74f97c4ef9785e769f26e3bc9f
Zoltan Csenyi <csoltan422@gmail.com>
2018-06-03 12:53:43 +0200

    Project init

---
+++ src/main/java/org/zenbot/timetableupdater/TimetableUpdaterApplication.java
@@ -0,0 +1,12 @@
+package org.zenbot.timetableupdater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetableUpdaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetableUpdaterApplication.class, args);
+	}
+}
