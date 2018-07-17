7813a44a16494b865e9e42539a0e2ee615161f41
Zoltan Csenyi <cszoltan422@gmail.com>
2018-07-17 20:12:44 +0200

    Update shell script

--- update-db.sh
+++ update-db.sh
@@ -3,4 +3,4 @@ java -version
 source ~/.profile
 echo $DB_UPDATER_HOME
 cd ${DB_UPDATER_HOME}
-mvn spring-boot:run -Dspring.data.mongodb.password=${SZOLNOK_DB_PASSWORD} -Dspring.profiles.active=$1
+SPRING_DATA_MONGODB_PASSWORD=${SZOLNOK_DB_PASSWORD} SPRING_PROFILES_ACTIVE=$1 gradle bootRun
